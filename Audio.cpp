ee46565f2bdcaf68b10204fe01431262b113346c
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-08-23 20:59:40 +0545

    fix: Optimised RealTime Transcription

--- Audio.cpp
+++ Audio.cpp
@@ -1,95 +1,85 @@
+#include <iostream>
+#include <mutex>
+#include <whisper.h>
+#include <portaudio.h>
+#include <dr_wav.h>
+
+#include "Globals.h"
 #include "Audio.h"
-#include "dr_wav.h"
-#include "whisper.h"
+#include "Modes.h"
+
+int MyRealTimeCallback(const void* inputBuffer, void* outputBuffer,
+	unsigned long framesPerBuffer,
+	const PaStreamCallbackTimeInfo* timeInfo,
+	PaStreamCallbackFlags statusFlags,
+	void* userData)
+{
+	Audio* audio = reinterpret_cast<Audio*>(userData);
+	return audio->RealTimeCallback(inputBuffer, outputBuffer, framesPerBuffer, timeInfo, statusFlags, nullptr); 
+}
 
 void Audio::ExitIfError()
 {
 	if (error != paNoError)
 	{
-		printf("PortAudio Error : %s", Pa_GetErrorText(error));
-		exit(EXIT_FAILURE);
-	}
-}
-
-auto Audio::GenerateCallback(Modes mode)
-{
-	switch (mode)
-	{
-	case Modes::RealTime:
-		return RealTimeCallback;
-		break;
-	case Modes::Subtitles:
-		return SubtitlesCallback;
-		break;
-	case Modes::Lyrics:
-		return LyricsCallback;
-		break;
-	default:
-		break;
+		std::cerr << "PortAudio Error: " << Pa_GetErrorText(error) << std::endl;
+		std::exit(EXIT_FAILURE);
 	}
 }
 
 int Audio::RealTimeCallback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData)
 {
-	//std::cout << "RT Callback called" << std::endl;
+	std::lock_guard<std::mutex> lock(queueMutex);
+
 	if (abortTranscription)
 		return paComplete;
 	if (!takeMicrophoneInput)
 		return paContinue;
+
 	const float* input = static_cast<const float*>(inputBuffer);
 
 	for (int i = 0; i < framesPerBuffer; i++)
 	{
-		pcm32_buffer.insert(pcm32_buffer.begin() + (count * PA_FRAMES_PER_BUFFER), 1, input[i]);
+		pcm32_input.push_back(input[i]);
+		count++;
 	}
-	count++;
-	if (pcm32_buffer.size() >= PA_SAMPLE_RATE * 2)
+
+	if (count >= 48000)
 	{
-		std::unique_lock<std::mutex> lock(bufferMutex);
-		pcm32_bufferQueue.push(pcm32_buffer);
+		audioQueue.push(pcm32_input);
+		queueCondition.notify_one();
+		pcm32_input.clear();
 		count = 0;
-		pcm32_buffer.clear();
-		bufferCV.notify_one();
 	}
 
 	return paContinue;
-}
-
-int Audio::SubtitlesCallback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData)
-{
-	// TODO: Implement this
-	return 0;
-}
-
-int Audio::LyricsCallback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData)
-{
-	// TODO: Implement this
-	return 0;
 }
 
 Audio::Audio()
 {
 	error = Pa_Initialize();
 	ExitIfError();
+
 	int numDevices = Pa_GetDeviceCount();
 	if (numDevices < 0)
 	{
-		printf("Error getting device count.\n");
-		exit(EXIT_FAILURE);
+		std::cerr << "PortAudio: Error Getting Device Count " << std::endl;
+		std::exit(EXIT_FAILURE);
 	}
 	else if (numDevices == 0)
 	{
-		printf("There are no available audio devices on this device.\n");
-		exit(EXIT_SUCCESS);
+		std::cerr << "PortAudio: There are no available audio devices on this device." << std::endl;
+		std::exit(EXIT_FAILURE);
 	}
-	std::cout << "Audio Intitialized" << std::endl;
+
+	std::cout << "PortAudio: Audio Intitialized" << std::endl;
 }
 
 void Audio::StartStream(Modes mode)
 {
 	std::cout << "Starting audio stream..." << std::endl;
-	//error = Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, PA_SAMPLE_RATE, PA_FRAMES_PER_BUFFER, GenerateCallback(mode), nullptr);
-	error = Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, PA_SAMPLE_RATE, PA_FRAMES_PER_BUFFER, RealTimeCallback, nullptr);
+	
+	error = Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, PA_SAMPLE_RATE, PA_FRAMES_PER_BUFFER, MyRealTimeCallback, this);
 	ExitIfError();
 
 	error = Pa_StartStream(stream);
