1d2d3ce3d1b2db84749e7d114de42c6f0f9b06b8
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-08-23 20:26:18 +0545

    Added Wave Feature

--- Globals.h
+++ Globals.h
@@ -1,18 +1,21 @@
 #pragma once
+
 #include <queue>
-#include <condition_variable>
 #include <mutex>
+#include <string>
+#include <raylib.h>
+#include <raylib-cpp/Font.hpp>
+#include <raylib-cpp/Text.hpp>
+#include <portaudio.h>
 
-#include <raylib-cpp.hpp>
-
-#include "Audio.h"
-#include "Transcriber.h"
 
 #define MGRAY CLITERAL(Color) {225, 225, 225, 255}
-#define MBG CLITERAL(Color) {26, 26, 36, 255}
+#define MPINK CLITERAL(Color) {255, 102, 196, 255}
+#define MTEXT CLITERAL(Color) {29, 43, 83, 255}
+#define MBG CLITERAL(Color) {238, 238, 238, 255}
+#define MBORDER CLITERAL(Color) {194, 195, 199, 255}
 #define LGRAY CLITERAL(Color) {73, 76, 93, 255}
 #define LIGHTBLUE CLITERAL(Color) {229, 241, 251, 255}
-// #define LIGHTGRAY CLITERAL(Color) {173, 173, 173, 255}
 
 const double PA_SAMPLE_RATE = 16000;
 const unsigned int PA_FRAMES_PER_BUFFER = 512;
@@ -22,14 +25,32 @@ extern bool abortTranscription;
 extern bool takeMicrophoneInput;
 extern int count;
 extern std::queue<std::vector<float>> pcm32_bufferQueue;
-extern std::vector<float> pcm32_buffer;
+extern std::vector<float> pcm32_input;
 extern std::condition_variable bufferCV;
 extern std::mutex bufferMutex;
 
 extern std::string transcribedText;
-// extern Transcriber* transcriber;
-// extern Audio* audio;
 
 //Load the Deafult font
 extern raylib::Font m_font;
-const std::string m_font_path = "./Resources/Fonts/OpenSans-SemiBold.ttf";
+extern raylib::Font raygui_font;
+extern raylib::Font m_font_title;
+
+const std::string m_font_path = "./Resources/Fonts/monogram.ttf";
+const std::string MODEL_PATH = "./Models/ggml-model-whisper-base.en.bin";
+
+// CallBack Function for PortAudio
+extern int MyRealTimeCallback(const void* inputBuffer, void* outputBuffer,
+	unsigned long framesPerBuffer,
+	const PaStreamCallbackTimeInfo* timeInfo,
+	PaStreamCallbackFlags statusFlags,
+	void* userData);
+
+struct TextWave
+{
+	int numCharacters;
+	float* characterOffset;
+
+};
+
+void addHoverEffect(raylib::Text, TextWave* wave, int x, int y);
