950fa1ec440c5727bd75c2f3393c8ff81b7b2849
Fiesty_Cushion <neupane.braz07@gmail.com>
2023-08-26 00:27:33 +0545

    Changed DLL Directory

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 # Copying the dlls
 $sourcePath = ".\Libraries\lib\"
-$destinationPath = ".\x64\Debug\"
+$destinationPath = "\build\VS2022\bin\x64\Debug"
 
 if (-Not (Test-Path -Path $destinationPath)) {
     New-Item -ItemType Directory -Force -Path $destinationPath | Out-Null
@@ -17,17 +17,17 @@ Write-Host "DLLs copied to $destinationPath" -ForegroundColor Green
 
 
 # Checking for the whisper model
-$filePath = ".\Models\ggml-model-whisper-base.en.bin"
+# $filePath = "..\Models\ggml-model-whisper-base.en.bin"
 
-$downloadURL = "https://ggml.ggerganov.com/ggml-model-whisper-base.en.bin"
-if (-Not(Test-Path -Path $filePath -PathType Leaf)) {
-    Write-Host "ERROR: The file '$filePath' does not exist." -ForegroundColor Red
-    Write-Host "Please download the file from the following URL:"
-    Write-Host $downloadURL
-    Write-Host "And place it under the path: $filePath"
-    return;
-}
-Write-Host "Whisper model file found!" -ForegroundColor Green
+# $downloadURL = "https://ggml.ggerganov.com/ggml-model-whisper-base.en.bin"
+# if (-Not(Test-Path -Path $filePath -PathType Leaf)) {
+#     Write-Host "ERROR: The file '$filePath' does not exist." -ForegroundColor Red
+#     Write-Host "Please download the file from the following URL:"
+#     Write-Host $downloadURL
+#     Write-Host "And place it under the path: $filePath"
+#     return;
+# }
+# Write-Host "Whisper model file found!" -ForegroundColor Green
 
 # MSBuild.exe .\Echo.sln /t:Build /p:Configuration=Debug
 
